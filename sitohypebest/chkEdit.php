2ba70dfab5ee25b9de35390705bfd42ca7d3a57e
ddauny <dani24iania@gmail.com>
2022-05-19 22:25:49 +0200

    agg

---
+++ sitohypebest/chkEdit.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include("login/connection.php");
+if (isset($_SESSION["idutente"])) { 
+    $uploaddir = 'img/';
+    $uploadfile = $uploaddir . basename($_FILES['img']['name']);
+
+    $password = $_POST["password"];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+   // $img = $_POST["img"];
+    $bio = $_POST["bio"];
+
+    if(isset($password) && $password != ""){
+        $sql = $conn->prepare("update utente set password = ? where ID = ?");
+        $sql->bind_param("si", md5($password), $_SESSION["idutente"]);
+        $sql->execute();
+    }
+
+    $sql = $conn->prepare("update utente set email = ?, username = ?, bio = ? where ID = ?");
+    $sql->bind_param("sssi", $email, $username, $bio, $_SESSION["idutente"]);
+    $sql->execute();
+
+    if($uploadfile != $uploaddir){
+        move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile);
+        $sql = $conn->prepare("update utente set img = ? where ID = ?");
+        $sql->bind_param("si", $uploadfile, $_SESSION["idutente"]);
+        $sql->execute();
+    }
+}
+?>
--- sitohypebest/edit.php
+++ sitohypebest/edit.php
@@ -4,18 +4,12 @@ session_start();
 include('login/connection.php');
 
 if (isset($_SESSION['idutente'])) {
-    $sql = 'select * from utente where ID = $_SESSION[idutente]';
+    $sql = "select * from utente where ID = $_SESSION[idutente]";
     $result = mysqli_query($conn, $sql);
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         echo "
     <form action='chkEdit.php' enctype='multipart/form-data' method='post'>
-
-    <label for='nome'>Nome</label>
-    <input type='text' placeholder='Nome' id='nome' name='nome' value=$row[nome]   required>
-
-    <label for='cognome'>Cognome</label>
-    <input type='text' placeholder='Cognome' id='cognome' name='cognome' value=$row[cognome] required>
 
     <label for='email'>Email</label>
     <input type='email' placeholder='Email' id='email' name='email' value=$row[email] required>
@@ -23,28 +17,18 @@ if (isset($_SESSION['idutente'])) {
     <label for='username'>Username</label>
     <input type='text' placeholder='Username' id='username' name='username' value=$row[username] required>
 
-
     <label for='password'>Password</label>
-    <input type='password' placeholder='Password' id='password' name='password' required>
-
-    <label for='data'>Data di nascita</label>
-    <input type='date' id='data' name='data' value=$row[data] min='1920-01-01' requirad>
-
-    <label for='sesso'>Sesso</label>
-    <select id='sesso' name='sesso' value=$row[sesso] required>
-        <option value=0>Maschio</option>
-        <option value=1>Femmina</option>
-        <option value=2>Preferisco non specificare</option>
-    </select>
+    <input type='password' placeholder='Password' id='password' name='password'>
 
     <label for='img'>Immagine profilo</label>
-    <input name='img' id='img' type='file' value=$row[img] required/>
+    <input name='img' id='img' type='file' value=$row[img]/>
 
     <label for='bio'>Biografia</label>
-    <input type='text' id='bio' name='bio' value=$row[bio] requirad> 
+    <input type='text' id='bio' name='bio' value='$row[bio]' required> 
+    <input type='submit' value='Modifica'>
     </form>";
     } else {
-        header('location"login.html');
+        header("location:profilo.php?idutente=$_SESSION[idutente]");
     }
 }
 ?>
