d8148270f4d5a3c0d1628ad69ea3e41bb02f4393
ddauny <dani24iania@gmail.com>
2022-05-21 19:11:18 +0200

    interesse

--- sitohypebest/home.php
+++ sitohypebest/home.php
@@ -25,11 +25,12 @@ include("login/connection.php");
     <link href="css/main.css" rel="stylesheet">
     <link href="css/tag.css" rel="stylesheet">
     <style>
-        .redIcon{
-            color:red;
+        .redIcon {
+            color: red;
         }
-        .blacIcon{
-            color:black;
+
+        .blacIcon {
+            color: black;
         }
     </style>
 </head>
@@ -107,8 +108,8 @@ include("login/connection.php");
                 <div class='card-body'>
                     <p class='card-text'><a class='disabledU' style='font-weight:bold;margin-left:0px' href='profilo.php?idutente=$row[idutente]'>$row[username] </a> $row[descrizione]</p>
                 ";
-                echo "<hr>";
-                
+                    echo "<hr>";
+
                     $sqlcommenti = "select testo, data, username, utente.ID as idutente from commenti inner join utente on IDUtente = utente.ID where IDPost = $row[idpost]";
                     $resultcommenti = mysqli_query($conn, $sqlcommenti);
                     if ($resultcommenti->num_rows > 0) {
@@ -129,88 +130,95 @@ include("login/connection.php");
                     echo "</div></div>";
                 }
             } else { //sono loggato ma non seguo nessuno, suggerisco i post dagli interessi
-                $sql = "select interesse.interesse as interesse from possiede join interesse on IDInteresse = interesse.ID where IDUtente = $_SESSION[idutente]";
+                $sql = "select interesse.ID as interesse from possiede join interesse on IDInteresse = interesse.ID where IDUtente = $_SESSION[idutente]";
                 $result = mysqli_query($conn, $sql);
-                $interessi = array();
-                while ($row = $result->fetch_assoc()) {
-                    array_push($interessi, $row["interesse"]);
-                }
 
-                foreach ($interessi as $interesse) {
-                    $sql = "select post.ID as idpost, username, post.img as imgpost, utente.ID as idutente, descrizione from post join utente on post.IDUtente = utente.ID where pubblicato = 1 and post.situazione like '%$interesse%' order by post.ID asc";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result->num_rows > 0) {
-                        while ($row = $result->fetch_assoc()) {
-                            echo "<div class='card' style='max-width:50%;min-width:460px; margin-left: auto; margin-right: auto;margin-bottom:10px;margin-top:20px'>
-                        <div style='margin-left: 6px;margin-top:6px'>
-                            <div class='card-title'>
-                            <img src='$row[imgutente]' class='card-img-top' style='width:40px;height:40px; border-radius:80%;'>
-                            <a class='disabledU' style='font-weight:bold;margin-left:5px' href='profilo.php?idutente=$row[idutente]'>$row[username]</a></div> 
-                        </div>
-                        <div><img src='$row[imgpost]' class='card-img-top' ></div> ";
-                            $sqllike = "select * from likes where IDPost = $row[idpost] and IDUtente = $_SESSION[idutente]";
-                            $resultlike = mysqli_query($conn, $sqllike);
-                            $liked = 0;
-                            $classlike = "fa-regular";
-                            if ($resultlike->num_rows > 0) {
-                                $liked = true;
-                                $classlike = "fa-solid redIcon";
-                            }
-        
-                            $sqllike = "select * from salva where IDPost = $row[idpost] and IDUtente = $_SESSION[idutente]";
-                            $resultlike = mysqli_query($conn, $sqllike);
-                            $saved = 0;
-                            $classsave = "blackIcon";
-                            if ($resultlike->num_rows > 0) {
-                                $saved = true;
-                                $classsave = "redIcon";
-                            }
-                            echo "
-                        <div style='position:relative;margin-top:5px; margin-right:5px' >
-                            <div style='float:right;'>
-                                <button class='border-0 bg-transparent' onclick='like($row[idpost],$liked)'><i id='like$row[idpost]' class='$classlike fa-heart fa-lg'></i></button>
-                                <button class='border-0 bg-transparent' onclick='save($row[idpost],$saved)'><i id='save$row[idpost]' class='fa $classsave fa-shirt fa-lg'></i></button>
-                                <div class='popup' onclick='popup()'><i style='margin-left:4px' class='fa fa-regular fa-tag fa-lg'></i>
-                                    <span class='popuptext' id='myPopup'>";
-        
-                            $sqltag = "select link, tipo, nome from tag join presenta on tag.ID = presenta.IDTag where presenta.IDPost = $row[idpost]";
-                            $resulttag = mysqli_query($conn, $sqltag);
-                            if ($resulttag->num_rows > 0) {
-                                while ($rowtag = $resulttag->fetch_assoc()) {
-                                    if ($rowtag["tipo"] == "profilo") { //se 0 è un tag utente
-                                        // echo $rowtag["link"];
-                                        $sqlutente = "select ID from utente where username = '$rowtag[link]'";
-                                        $resultutente = mysqli_query($conn, $sqlutente);
-                                        if ($resultutente->num_rows > 0) {
-                                            $rowutente = $resultutente->fetch_assoc();
-                                            $utentetaggato = $rowutente["ID"];
-                                        }
-                                        echo "<a class='disabledU' style='font-weight:bold'  href='profilo.php?idutente=$utentetaggato'>$rowtag[link]</a>";
-                                    } else { //sto taggando un articolo
-                                        echo "<a  class='disabledU' style='font-weight:bold'  href='$rowtag[link]'>$rowtag[nome]</a>";
-                                    }
-                                    echo "<br>";
-                                }
-                            }
-                            echo "</span>
-                                </div>
-                                
-                            </div>
-                        </div>
-                        <div class='card-body'>
-                            <p class='card-text'><a class='disabledU' style='font-weight:bold;margin-left:0px' href='profilo.php?idutente=$row[idutente]'>$row[username] </a> $row[descrizione]</p>
-                        ";
-                            $sqlcommenti = "select testo, data, username, utente.ID as idutente from commenti inner join utente on IDUtente = utente.ID where IDPost = $row[idpost]";
-                            $resultcommenti = mysqli_query($conn, $sqlcommenti);
-                            if ($resultcommenti->num_rows > 0) {
-                                echo "<p>Commenti:</p>";
-                                while ($row = $resultcommenti->fetch_assoc()) {
-                                    echo "<div class='card-text'><a class='disabledU' style='font-weight:bold;margin-left:0px' href='profilo.php?idutente=$row[idutente]'>$row[username]</a> <label for='$row[username]'>$row[testo]</label> <label>$row[data]</label></div>";
-                                }
-                            }
-        
-                            echo "</div></div>";
+                $row = $result->fetch_assoc();
+                $interesse = $row["interesse"];
+                $sql = "select post.ID as idpost, username, post.img as imgpost, utente.ID as idutente, descrizione, utente.img as imgutente from post join utente on post.IDUtente = utente.ID where pubblicato = 1 and post.situazione = $interesse order by post.ID asc";
+                $result = mysqli_query($conn, $sql);
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $idpost = $row["idpost"];
+                        echo "<div class='card postino'>
+                <div style='margin-left: 6px;margin-top:6px'>
+                    <div class='card-title'>
+                    <img src='$row[imgutente]' class='card-img-top' style='width:40px;height:40px; border-radius:80%;'>
+                    <a class='disabledU' style='font-weight:bold;margin-left:5px' href='profilo.php?idutente=$row[idutente]'>$row[username]</a></div> 
+                </div>
+                <div><img src='$row[imgpost]' class='card-img-top' ></div> ";
+                        $sqllike = "select * from likes where IDPost = $row[idpost] and IDUtente = $_SESSION[idutente]";
+                        $resultlike = mysqli_query($conn, $sqllike);
+                        $liked = 0;
+                        $classlike = "fa-regular";
+                        if ($resultlike->num_rows > 0) {
+                            $liked = true;
+                            $classlike = "fa-solid redIcon";
                         }
+
+                        $sqllike = "select * from salva where IDPost = $row[idpost] and IDUtente = $_SESSION[idutente]";
+                        $resultlike = mysqli_query($conn, $sqllike);
+                        $saved = 0;
+                        $classsave = "blackIcon";
+                        if ($resultlike->num_rows > 0) {
+                            $saved = true;
+                            $classsave = "redIcon";
+                        }
+                        echo "
+                <div style='position:relative;margin-top:5px; margin-right:5px' >
+                    <div style='float:right;'>
+                        <button style='position:absloute; left:2px'class='border-0 bg-transparent' onclick='like($row[idpost],$liked)'><i id='like$row[idpost]' class='$classlike fa-heart fa-lg'></i></button></>
+                        <button class='border-0 bg-transparent' onclick='save($row[idpost],$saved)'><i id='save$row[idpost]' class='fa $classsave fa-shirt fa-lg'></i></button>
+                        <div class='popup' onclick='popup($row[idpost])'><i style='margin-left:4px' class='fa fa-regular fa-tag fa-lg'></i>
+                            <span class='popuptext' id='myPopup$row[idpost]'>";
+
+                        $sqltag = "select link, tipo, nome from tag join presenta on tag.ID = presenta.IDTag where presenta.IDPost = $row[idpost]";
+                        $resulttag = mysqli_query($conn, $sqltag);
+                        if ($resulttag->num_rows > 0) {
+                            while ($rowtag = $resulttag->fetch_assoc()) {
+                                if ($rowtag["tipo"] == "profilo") { //se 0 è un tag utente
+                                    // echo $rowtag["link"];
+                                    $sqlutente = "select ID from utente where username = '$rowtag[link]'";
+                                    $resultutente = mysqli_query($conn, $sqlutente);
+                                    if ($resultutente->num_rows > 0) {
+                                        $rowutente = $resultutente->fetch_assoc();
+                                        $utentetaggato = $rowutente["ID"];
+                                    }
+                                    echo "<a class='disabledU' style='font-weight:bold'  href='profilo.php?idutente=$utentetaggato'>$rowtag[link]</a>";
+                                } else { //sto taggando un articolo
+                                    echo "<a  class='disabledU' style='font-weight:bold'  target='_blanck'href='$rowtag[link]'>$rowtag[nome]</a>";
+                                }
+                                echo "<br>";
+                            }
+                        }
+                        echo "</span>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class='card-body'>
+                    <p class='card-text'><a class='disabledU' style='font-weight:bold;margin-left:0px' href='profilo.php?idutente=$row[idutente]'>$row[username] </a> $row[descrizione]</p>
+                ";
+                        echo "<hr>";
+
+                        $sqlcommenti = "select testo, data, username, utente.ID as idutente from commenti inner join utente on IDUtente = utente.ID where IDPost = $row[idpost]";
+                        $resultcommenti = mysqli_query($conn, $sqlcommenti);
+                        if ($resultcommenti->num_rows > 0) {
+                            echo "<div class='commentiDiv' >";
+                            while ($row = $resultcommenti->fetch_assoc()) {
+                                echo "<div class='card-text'><a class='disabledU' style='font-weight:bold;margin-left:0px' href='profilo.php?idutente=$row[idutente]'>$row[username]</a> <label  for='$row[username]'>$row[testo]</label> <label style='font-size:10px;float:right'>$row[data]</label></div>";
+                            }
+                            echo "</div>";
+                        }
+                        // INIZIA LA PARTE PER COMMENTAREEE
+                        echo "
+                    <div style='margin-top:8px'>
+                    <form role='search' action='post/comments.php?idpost=$idpost' method='post'>
+                        <input type='search' class='buttonCommento' name='commento' placeholder='Aggiungi un commento...' />
+                        <input type='submit' class='buttonPubblica' value='Pubblica'/>
+                    </form>
+                    </div>";
+                        echo "</div></div>";
                     }
                 }
             }
