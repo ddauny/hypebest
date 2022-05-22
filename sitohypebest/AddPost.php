abd6dcc9140cb836bdbcdc498f001ffb2cdb4cd9
salbeoo <albertostagnolavoro@gmail.com>
2022-05-22 11:31:12 +0200

    asa

--- sitohypebest/AddPost.php
+++ sitohypebest/AddPost.php
@@ -5,7 +5,11 @@
 <head>
     <title>HypeBest - Add Post</title>
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="description" content="">
 
+    
     <link rel="stylesheet" href="css/styleAddPost.css" type="text/css">
     <script src="js/addTag.js"></script>
 
@@ -14,7 +18,7 @@
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/headers/">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link href="css/headers.css" rel="stylesheet">
@@ -37,20 +41,20 @@
             <form action="chkAddPost.php" enctype="multipart/form-data" method="post">
                 <div class="container">
 
-                    <label for="descrizione"><b>Descrizione<b></label>
+                    <label class="labelB" for="descrizione">Descrizione</label>
                     <input type="text" placeholder="Descrizione" id="descrizione" name="descrizione" required>
 
-                    <label for="sesso"><b>Sesso<b></label><br>
+                    <label class="labelB" for="sesso">Sesso</label><br>
                     <select id="sesso" name="sesso" required>
                         <option value=0>Maschio</option>
                         <option value=1>Femmina</option>
                         <option value=2>Preferisco non specificare</option>
                     </select>
 
-                    <br><label for="img"><b>Immagine<b></label>
+                    <br><label  class="labelB" for="img">Immagine</label>
                     <input name="img" id="img" type="file" required />
 
-                    <br><label for="interesse"><b>Situazione</b></label><br>
+                    <br><label class="labelB" for="interesse">Situazione</label><br>
                     <?php
                     $sql = "select * from interesse";
                     $result = mysqli_query($conn, $sql);
@@ -64,7 +68,7 @@
                     ?>
 
                     <div class="input-group" id="container-input">
-                        <span class="input-group">Tag e nome tag</span>
+                        <span class="input-group labelB">Tag e nome tag</span>
                         <input type="text" placeholder="@utente o link prodotto" id="tag" name="tag[]" class="form-control">
                         <input type="text" placeholder="nome da visualizzare" id="nometag" name="nometag[]" class="form-control">
                         
@@ -86,6 +90,9 @@
             </form>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
