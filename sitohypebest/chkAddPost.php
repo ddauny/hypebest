2e4bf1a3badced73098a8214e32e8eb235805a03
ddauny <dani24iania@gmail.com>
2022-05-22 11:18:01 +0200

    fine
    la fine è arrivata

--- sitohypebest/chkAddPost.php
+++ sitohypebest/chkAddPost.php
@@ -7,59 +7,54 @@ $descrizione = $_POST["descrizione"];
 $sesso = $_POST["sesso"];
 
 
-
-
-
-
-
-
-
 $uploaddir = 'img/';
 $uploadfile = $uploaddir . basename($_FILES['img']['name']);
-move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile);
+if ($uploadfile != $uploaddir) {
+    move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile);
 
 
-$sql = $conn->prepare("INSERT INTO post (IDUtente, situazione, descrizione, sesso, img) VALUES (?, ?, ?, ?, ?)");
-$sql->bind_param("issis", $_SESSION["idutente"], $situazione, $descrizione, $sesso, $uploadfile);
+    $sql = $conn->prepare("INSERT INTO post (IDUtente, situazione, descrizione, sesso, img) VALUES (?, ?, ?, ?, ?)");
+    $sql->bind_param("issis", $_SESSION["idutente"], $situazione, $descrizione, $sesso, $uploadfile);
 
 
-if ($sql->execute() === TRUE) {
-    $nametags = $_POST['nometag'];
-    $tags = $_POST['tag'];
- //   echo $nametags;
-    if (isset($nametags)) {
-        $arraytag = array(); //array dei tag
-        foreach ($tags as $tag) {
-            array_push($arraytag, $tag);
-        }
-        if (count($arraytag) > 0) {
-            $sql = "select max(ID) from post";
-            $result = mysqli_query($conn, $sql);
-            $row = $result->fetch_assoc();
-            $lastidpost = $row["max(ID)"];
-            $arraynome = array();
-            foreach ($nametags as $nametag) {
-                array_push($arraynome, $nametag);
+    if ($sql->execute() === TRUE) {
+        $nametags = $_POST['nometag'];
+        $tags = $_POST['tag'];
+        //   echo $nametags;
+        if (isset($nametags)) {
+            $arraytag = array(); //array dei tag
+            foreach ($tags as $tag) {
+                array_push($arraytag, $tag);
             }
-            for ($i = 0; $i < count($arraytag); $i++) {
-                $sql = $conn->prepare("INSERT INTO tag (link, tipo, nome) VALUES (?, ?, ?)");
-                $tipo = "articolo";
-                if (substr($arraytag[$i], 0, 1) == "@") {
-                    $tipo = "profilo";
+            if (count($arraytag) > 0) {
+                $sql = "select max(ID) from post";
+                $result = mysqli_query($conn, $sql);
+                $row = $result->fetch_assoc();
+                $lastidpost = $row["max(ID)"];
+                $arraynome = array();
+                foreach ($nametags as $nametag) {
+                    array_push($arraynome, $nametag);
                 }
-                $sql->bind_param("sss", substr($arraytag[$i],1,strlen($arraytag[$i]) - 1), $tipo, $arraynome[$i]);
-                $sql->execute();
-                $lastidtag = $sql->insert_id;
-                $sql = $conn->prepare("INSERT INTO presenta (IDPost, IDTag) VALUES (?, ?)");
-                echo $lastidpost, $lastidtag;
-                $sql->bind_param("ii", $lastidpost, $lastidtag);
-                $sql->execute();
+                for ($i = 0; $i < count($arraytag); $i++) {
+                    $sql = $conn->prepare("INSERT INTO tag (link, tipo, nome) VALUES (?, ?, ?)");
+                    $tipo = "articolo";
+                    if (substr($arraytag[$i], 0, 1) == "@") {
+                        $tipo = "profilo";
+                    }
+                    $sql->bind_param("sss", substr($arraytag[$i], 1, strlen($arraytag[$i]) - 1), $tipo, $arraynome[$i]);
+                    $sql->execute();
+                    $lastidtag = $sql->insert_id;
+                    $sql = $conn->prepare("INSERT INTO presenta (IDPost, IDTag) VALUES (?, ?)");
+                    echo $lastidpost, $lastidtag;
+                    $sql->bind_param("ii", $lastidpost, $lastidtag);
+                    $sql->execute();
+                }
             }
         }
+        header("location:profilo.php");
     }
-    header("location:profilo.php");
 } else {
-    header("location:AddPost.html");
+    header("location:AddPost.php");
 }
 $sql->close();
 $conn->close();
---
+++ sitohypebest/filtered.php
@@ -0,0 +1,128 @@
+<head>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js%22%3E"></script>
+    <script src="js/featurePost.js"></script>
+    <title>HypeBest - Filtered</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/bc3de12e9c.js" crossorigin="anonymous"></script>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/headers/">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="js/featurePost.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="css/headers.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/tag.css" rel="stylesheet">
+    <script src="js/tag.js"></script>
+</head>
+
+<?php
+session_start();
+include("login/connection.php");
+include("navBar.php");
+if (isset($_SESSION["idutente"]) && isset($_POST["selected"])) {
+    $idutente = $_SESSION["idutente"];
+    $selected = $_POST["selected"];
+    $sql = "select username, ID from utente where username = '$selected'";
+    $result = mysqli_query($conn, $sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        header("location:profilo.php?idutente=$row[ID]");
+    } else {
+        $sql = "select post.ID as idpost, username, post.img as imgpost, utente.ID as idutente, descrizione, utente.img as imgutente from post join utente on post.IDUtente = utente.ID where post.situazione like '%$selected%' and pubblicato = 1";
+        $result = mysqli_query($conn, $sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $idpost = $row["idpost"];
+                echo "<div class='card postino'>
+                <div style='margin-left: 6px;margin-top:6px'>
+                    <div class='card-title'>
+                    <img src='$row[imgutente]' class='card-img-top' style='width:40px;height:40px; border-radius:80%;'>
+                    <a class='disabledU' style='font-weight:bold;margin-left:5px' href='profilo.php?idutente=$row[idutente]'>$row[username]</a></div>";
+                if ($_SESSION["idutente"] == $row["idutente"]) {
+                    echo "<a class='disabledU' href='post/delete.php?idpost=$row[idpost]' style='position:absolute;right:10px;top:26px'><i class='fa-solid fa-xmark fa-2xl'></i></a>";
+                }
+                echo   "
+                    </div>
+                <div><img src='$row[imgpost]' class='card-img-top' ></div> ";
+                $sqllike = "select * from likes where IDPost = $row[idpost] and IDUtente = $_SESSION[idutente]";
+                $resultlike = mysqli_query($conn, $sqllike);
+                $liked = 0;
+                $classlike = "fa-regular";
+                if ($resultlike->num_rows > 0) {
+                    $liked = true;
+                    $classlike = "fa-solid redIcon";
+                }
+
+                $sqllike = "select * from salva where IDPost = $row[idpost] and IDUtente = $_SESSION[idutente]";
+                $resultlike = mysqli_query($conn, $sqllike);
+                $saved = 0;
+                $classsave = "blackIcon";
+                if ($resultlike->num_rows > 0) {
+                    $saved = true;
+                    $classsave = "redIcon";
+                }
+                echo "
+                <div style='position:relative;margin-top:5px; margin-right:5px' >
+                    <div style='float:right;'>
+                        <button style='position:absloute; left:2px'class='border-0 bg-transparent' onclick='like($row[idpost],$liked)'><i id='like$row[idpost]' class='$classlike fa-heart fa-lg'></i></button></>
+                        <button class='border-0 bg-transparent' onclick='save($row[idpost],$saved)'><i id='save$row[idpost]' class='fa $classsave fa-shirt fa-lg'></i></button>
+                        <div class='popup' onclick='popup($row[idpost])'><i style='margin-left:4px' class='fa fa-regular fa-tag fa-lg'></i>
+                            <span class='popuptext' id='myPopup$row[idpost]'>";
+
+                $sqltag = "select link, tipo, nome from tag join presenta on tag.ID = presenta.IDTag where presenta.IDPost = $row[idpost]";
+                $resulttag = mysqli_query($conn, $sqltag);
+                if ($resulttag->num_rows > 0) {
+                    while ($rowtag = $resulttag->fetch_assoc()) {
+                        if ($rowtag["tipo"] == "profilo") { //se 0 è un tag utente
+                            // echo $rowtag["link"];
+
+                            $sqlutente = "select ID from utente where username = '$rowtag[link]'";
+                            $resultutente = mysqli_query($conn, $sqlutente);
+                            if ($resultutente->num_rows > 0) {
+                                $rowutente = $resultutente->fetch_assoc();
+                                $utentetaggato = $rowutente["ID"];
+                            }
+                            echo "<a class='disabledU' style='font-weight:bold'  href='profilo.php?idutente=$utentetaggato'>$rowtag[link]</a>";
+                        } else { //sto taggando un articolo
+                            // echo "sdajkdcbkjas";
+                            echo $rowtag["tipo"];
+                            echo "<a  class='disabledU' style='font-weight:bold'  target='_blanck'href='$rowtag[link]'>$rowtag[nome]</a>";
+                        }
+                        echo "<br>";
+                    }
+                }
+                echo "</span>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class='card-body'>
+                    <p class='card-text'><a class='disabledU' style='font-weight:bold;margin-left:0px' href='profilo.php?idutente=$row[idutente]'>$row[username] </a> $row[descrizione]</p>
+                ";
+                echo "<hr>";
+
+                $sqlcommenti = "select testo, data, username, utente.ID as idutente from commenti inner join utente on IDUtente = utente.ID where IDPost = $row[idpost]";
+                $resultcommenti = mysqli_query($conn, $sqlcommenti);
+                if ($resultcommenti->num_rows > 0) {
+                    echo "<div class='commentiDiv' >";
+                    while ($row = $resultcommenti->fetch_assoc()) {
+                        echo "<div class='card-text'><a class='disabledU' style='font-weight:bold;margin-left:0px' href='profilo.php?idutente=$row[idutente]'>$row[username]</a> <label  for='$row[username]'>$row[testo]</label> <label style='font-size:10px;float:right'>$row[data]</label></div>";
+                    }
+                    echo "</div>";
+                }
+                // INIZIA LA PARTE PER COMMENTAREEE
+                echo "
+                    <div style='margin-top:8px'>
+                    <form role='search' action='post/comments.php?idpost=$idpost' method='post'>
+                        <input type='search' class='buttonCommento' name='commento' placeholder='Aggiungi un commento...' />
+                        <input type='submit' class='buttonPubblica' value='Pubblica'/>
+                    </form>
+                    </div>";
+                echo "</div></div>";
+            }
+        }
+    }
+}
+?>
