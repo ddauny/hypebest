1bbbed6581cae9c4ba00d29c6451ffa7b9cd7e5d
ddauny <dani24iania@gmail.com>
2022-05-22 15:48:21 +0200

    fix beta

--- sitohypebest/login/chkRegistrazione.php
+++ sitohypebest/login/chkRegistrazione.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION["idutente"])) {
     $uploaddir = '../img/';
     $uploadfile = $uploaddir . basename($_FILES['img']['name']);
     $upv = 'img/';
-    $up = $upv .basename($_FILES['img']['name']);
+    $up = $upv . basename($_FILES['img']['name']);
 
     $password = $_POST["password"];
     $cognome = $_POST["cognome"];
@@ -20,22 +20,25 @@ if (!isset($_SESSION["idutente"])) {
 
     if (move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile)) {
         $password = md5($password);
-        $sql = "insert into utente (nome, cognome, email, username, dataNascita, sesso, img, bio, ruolo, password) values ('$nome', '$cognome', '$email', '$username', '$data', '$sesso', '$up', '$bio', '0', '$password')";
-        try{
-        if ($conn->query($sql) === true) { //ho inserito nel db
-            $sql = $conn->prepare("select ID from utente where ? = username");
-            $sql->bind_param("s", $username);
-            $sql->execute();
-            $row = $result->fetch_assoc();
-            $_SESSION["idutente"] = $row["ID"];
-            echo "cvwopdvcwp";
-            $sql = "insert into possiede (IDInteresse, IDUtente) values ('$interesse', '$_SESSION[idutente]')";
-            $conn->query($sql);
-            header("location:../index.php");
-        } else {
-            header("location:registrazione.php");
-        }
-        } catch (Exception $e){
+        $sql = $conn->prepare("insert into utente (nome, cognome, email, username, dataNascita, sesso, img, bio, ruolo, password) values (?,?,?,?,?,?,?,?,?,?)");
+        $zero = '0';
+        $sql->bind_param("sssssissss", $nome, $cognome, $email, $username, $data, $sesso, $up, $zero, $password);
+        $sql->execute();
+        try {
+            if ($conn->query($sql) === true) { //ho inserito nel db
+                $sql = $conn->prepare("select ID from utente where ? = username");
+                $sql->bind_param("s", $username);
+                $sql->execute();
+                $row = $result->fetch_assoc();
+                $_SESSION["idutente"] = $row["ID"];
+                echo "cvwopdvcwp";
+                $sql = "insert into possiede (IDInteresse, IDUtente) values ('$interesse', '$_SESSION[idutente]')";
+                $conn->query($sql);
+                header("location:../index.php");
+            } else {
+                header("location:registrazione.php");
+            }
+        } catch (Exception $e) {
             header("location:registrazione.php");
         }
     } else {
@@ -44,3 +47,4 @@ if (!isset($_SESSION["idutente"])) {
 } else {
     header("location:../home.php");
 }
+?>
